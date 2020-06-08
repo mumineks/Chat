b264ae4291a124d06159effc7fb8e067a835aaf0
mumineks <52258482+mumineks@users.noreply.github.com>
2020-06-08 19:02:24 +0200

    chat with classes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import {Route, Switch} from 'react-router-dom'
+import Home from './Home'
+import Chat from './Chat'
 
-function App() {
+
+class App extends Component{
+  constructor(props){
+      super(props);
+      this.state = {name: 'Andrzej',
+                  quotes: []
+    }
+      this.setName = this.setName.bind(this);
+      this.newQuote = this.newQuote.bind(this);
+  }
+ setName(val){
+          console.log('Submit')
+          this.setState({
+            name: val.userName
+          });
+
+      }
+  newQuote(x){
+    let oldQuotes = this.state.quotes
+    this.setState({
+      quotes: [...oldQuotes, x.quote]
+    })
+  }
+  render(){
+      return(
+          <div className = 'App'>
+             <Route exact path ='/' render = { (routeProps) => <Home name = {this.state.name} setName = {this.setName} {...routeProps}/> }  />
+              <Route exact path ='/chat' render = {() => <Chat name = {this.state.name} quotes = {this.state.quotes} newQuote = {this.newQuote}/>}/>
+          </div>
+      )
+  }
+}
+
+
+/*function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+          <Route exact path ='/' component ={Home} />
+          <Route exact path ='/chat' component ={Chat} />
     </div>
   );
-}
+}*/
 
 export default App;
